3b0281e71b6ba03e3ddb2e530435681fae0b2684
SufiAzarquiel <alumno.745709@ies-azarquiel.es>
2022-10-29 11:05:57 +0200

    Create hastacero.java

---
+++ src/main/java/com/github/sufiazarquiel/myjavaworkspace/buclewhile/hastacero.java
@@ -0,0 +1,22 @@
+package com.github.sufiazarquiel.myjavaworkspace.buclewhile;
+
+import java.util.Scanner;
+
+public class hastacero {
+    /*
+	 * HastaCero : bucle while que lee numeros hasta que
+     * el usuario introduce un 0.
+	 */
+	public static void main(String[] args) {
+		// Variables
+		int var = 1; // Descripción opcional de variable
+
+		// Objeto para leer lo que teclea el usuario.
+		Scanner teclado = new Scanner(System.in);
+		while(var != 0) {
+			System.out.print("Teclea un 0: ");
+			var = teclado.nextInt();
+		}
+		teclado.close();
+	}
+}
