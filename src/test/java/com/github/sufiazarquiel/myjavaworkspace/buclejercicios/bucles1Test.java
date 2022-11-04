ed6c79a68c3cbd565f108c1ac7d8c7004a13a58b
SufiAzarquiel <alumno.745709@ies-azarquiel.es>
2022-11-04 10:32:58 +0100

    Create bucles1Test.java

---
+++ src/test/java/com/github/sufiazarquiel/myjavaworkspace/buclejercicios/bucles1Test.java
@@ -0,0 +1,15 @@
+package com.github.sufiazarquiel.myjavaworkspace.buclejercicios;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit-level testing for {@link bucles1} object.
+ */
+public class bucles1Test {
+    @Test
+    void testMain() {
+        bucles1 main = new bucles1();
+        assertNotNull(main);
+    }
+}
