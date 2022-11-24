b741e2155787b969dbd9c2be10cccd427c57c5b8
SufiAzarquiel <alumno.745709@ies-azarquiel.es>
2022-11-24 09:31:42 +0100

    Refactoring

--- src/test/java/com/github/sufiazarquiel/myjavaworkspace/examen/cifraslong.java
+++ src/test/java/com/github/sufiazarquiel/myjavaworkspace/examen/cifraslong.java
@@ -2,24 +2,24 @@ package com.github.sufiazarquiel.myjavaworkspace.examen;
 
 import java.util.Scanner;
 
-public class cifrasordenadas {
+public class cifraslong {
     /*
      * Ejercicio n: Descripcion
      */
 
     // Método que hace la tarea que pide el ejercicio.
-    public static int cifrasEnOrden(String input) {
+    public static long cifrasEnOrdenLong(String input) {
         // Variables
-        int num = 0; // El número introducido por el usuario
-        int n = 0; // Variable usada en el bucle
-        int cifra = 0; // Cada cifra del numero introducido
+        long num = 0; // El número introducido por el usuario
+        long n = 0; // Variable usada en el bucle
+        long cifra = 0; // Cada cifra del numero introducido
         boolean ordenado = true; // Verdadero si el numero resulta estar en orden ascendente
 
         // Objeto para leer lo que teclea el usuario.
         try {
             Scanner teclado = new Scanner(input);
             System.out.print("Introduce un número: ");
-            num = teclado.nextInt();
+            num = teclado.nextLong();
             n = num; // n empieza siendo igual al número introducido
             cifra = n % 10; // Empezamos por la primera cifra, que sera el resto del número entre 10
             System.out.print(num + "\n");
@@ -33,7 +33,7 @@ public class cifrasordenadas {
 
         // Un bucle hasta la última cifra.
         do {
-            int prev = cifra;
+            long prev = cifra;
             cifra = n % 10;
             n /= 10;
             if (prev < cifra) {
@@ -52,12 +52,12 @@ public class cifrasordenadas {
     // Ejecuta el método principal de la clase.
     public static void main(String[] args) {
         // Valores que simulan input del usuario.
-        String input = "2456\n" + "-1\n";
+        String input = "1234566789\n" + "-1\n";
 
         // Ejecutar función del ejercicio.
-        cifrasEnOrden(input);
+        cifrasEnOrdenLong(input);
 
-        input = "4637\n" + "-1\n";
-        cifrasEnOrden(input);
+        input = "466666666637\n" + "-1\n";
+        cifrasEnOrdenLong(input);
     }
 }
